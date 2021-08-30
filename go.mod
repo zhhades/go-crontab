0a1b473c6c375e000c025d145c05cdd43eb2aa06
zhhades <zhhades@foxmail.com>
2021-08-30 23:00:53 +0800

    init code

---
+++ go.mod
@@ -0,0 +1,10 @@
+module crontab
+
+go 1.16
+
+require (
+	github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75
+	go.etcd.io/etcd/api/v3 v3.5.0
+	go.etcd.io/etcd/client/v3 v3.5.0
+	go.mongodb.org/mongo-driver v1.7.1
+)
---
+++ master/LogMgr.go
@@ -0,0 +1,77 @@
+package master
+
+import (
+	"context"
+	"crontab/common"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+// mongodb日志管理
+type LogMgr struct {
+	client        *mongo.Client
+	logCollection *mongo.Collection
+}
+
+var (
+	G_logMgr *LogMgr
+)
+
+func InitLogMgr() (err error) {
+	var (
+		client *mongo.Client
+	)
+
+	// 建立mongodb连接
+	clientOptions := options.Client().ApplyURI(G_config.MongodbUri).SetConnectTimeout(time.Duration(G_config.MongodbConnectTimeout) * time.Millisecond)
+	if client, err = mongo.Connect(
+		context.TODO(), clientOptions); err != nil {
+		return
+	}
+
+	G_logMgr = &LogMgr{
+		client:        client,
+		logCollection: client.Database("cron").Collection("log"),
+	}
+	return
+}
+
+// 查看任务日志
+func (logMgr *LogMgr) ListLog(name string, skip int, limit int) (logArr []*common.JobLog, err error) {
+	var (
+		filter  *common.JobLogFilter
+		logSort *common.SortLogByStartTime
+		cursor  *mongo.Cursor
+		jobLog  *common.JobLog
+	)
+
+	// len(logArr)
+	logArr = make([]*common.JobLog, 0)
+
+	// 过滤条件
+	filter = &common.JobLogFilter{JobName: name}
+
+	// 按照任务开始时间倒排
+	logSort = &common.SortLogByStartTime{SortOrder: -1}
+
+	var finds = options.Find()
+	// 查询
+	if cursor, err = logMgr.logCollection.Find(context.TODO(), filter, finds.SetSort(logSort), finds.SetSkip(int64(skip)), finds.SetLimit(int64(limit))); err != nil {
+		return
+	}
+	// 延迟释放游标
+	defer cursor.Close(context.TODO())
+
+	for cursor.Next(context.TODO()) {
+		jobLog = &common.JobLog{}
+
+		// 反序列化BSON
+		if err = cursor.Decode(jobLog); err != nil {
+			continue // 有日志不合法
+		}
+
+		logArr = append(logArr, jobLog)
+	}
+	return
+}
